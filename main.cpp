ea573d8795f63ba937ed9138e485668b867499c2
huangmin007 <hm-secret@163.com>
2019-06-14 02:51:17 +0000

    add pthread handler

--- main.cpp
+++ main.cpp
@@ -18,11 +18,17 @@
 
 #include "TCPServer.h"
 
-// sigaction handler
-int runningi = 1;	//rinning
+
+int running = 1;
+TCPServer server;
+
+
 void ctrl_c_handler(int signum)
 {
-	runningi = 0;
+	printf("signum:%d\n", signum);
+	if(signum == SIGPIPE)	return;
+
+	running = 0;
 }
 void setup_sigaction()
 {
@@ -30,56 +36,74 @@ void setup_sigaction()
 	sa.sa_handler = ctrl_c_handler;
 	sigaction(SIGINT, &sa, NULL);
 	sigaction(SIGTERM, &sa, NULL);
+	sigaction(SIGPIPE, &sa, NULL);
+
+	//signal(SIGPIPE, SIG_IGN);
 }
 
-TCPServer server;
+static void *GetAbsPosition(void *args)
+{
+	Window root = 0;
+	Window window = 0;
+	Display *display = XOpenDisplay(0);
+
+	int x = 0, y = 0;
+	int dummy_int = 0;
+	unsigned int dummy_uint = 0;
+
+	char res[32];
+	uint8_t buf[30];
+
+	int fd = open("/dev/input/mouse0", O_RDONLY);
+	if(fd < 0)
+	{
+		printf("failed to open \"/dev/input\"\n");
+		exit(1);
+	}
+
+	while(1)
+	{
+		if(read(fd, buf, sizeof(buf)) > 0)
+		{
+			int ret = XQueryPointer(display, XDefaultRootWindow(display),
+				&root, &window, &x, &y, &dummy_int, &dummy_int, &dummy_uint);
+			if(ret <= 0)	continue;
+
+			sprintf(res, "{\"type\":0x%02x, \"rx\":%d, \"ry\":%d, \"ax\":%d, \"ay\":%d}\r\n", buf[0], buf[1], buf[2], x, y); 
+			printf("%s", res);
+
+			server.Send((uint8_t*)res, strlen(res));
+			usleep(30 * 1000);
+		}
+		//usleep(30 * 1000);
+	}
+}
+
 
 int main(int argc, char *argv[])
 {
 	setup_sigaction();
 	printf("start ... \n");
-	int x = 0, y = 0;
-
-	Display *display = XOpenDisplay(0);
-	Window window = 0;
-	Window root = 0;
-
-	int dummy_int = 0;
-	unsigned int dummy_uint = 0;
-	
-	uint8_t buf[3];
-	int fd = open("/dev/input/mouse0", O_RDONLY);
-	
-	if(fd < 0)
-	{
-		printf("failed to open \"/dev/input/\"\n");
-		exit(-1);
-	}
 
 	server.Bind(5000);
 	server.Start();
 
-	char res[32];
-
-	while(runningi)
+	pthread_t pt_id;
+    int ret	= pthread_create(&pt_id, NULL, GetAbsPosition, NULL);
+	if(ret != 0)
 	{
-		if(read(fd, buf, sizeof(buf)) > 0)
-		{
-			int ret = XQueryPointer(display, XDefaultRootWindow(display), 
-				&root, &window, &x, &y, &dummy_int, &dummy_int, &dummy_uint);
+		printf("get abs position pthread start failed ... \n");
+		exit(1);
+	}
 
-			if(ret <= 0) continue;
-			//printf("mouse type=0x%02x ax=%d ay=%d\r\n", buf[0], x, y);
-			//printf("mouse type=0x%02x relX=%d relY=%d  absX=%d absY=%d\r\n", buf[0], buf[1], buf[2], x, y);
-			
-			sprintf(res, "{\"type\":0x%02x, \"rx\":%d, \"ry\":%d, \"ax\":%d, \"ay\":%d}\r\n", buf[0], buf[1], buf[2], x, y); 
-			printf("%s", res);
-			server.Send((uint8_t*)res, strlen(res));
-			
-		}
-		usleep(50 * 1000);
+	while(running)
+	{
+		sleep(1);
 	}
 
 	server.Stop();
+	pthread_cancel(pt_id);
+	
 	return 0;
 }
+
